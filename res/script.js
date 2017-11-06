26a13f81840b952f620d79e8748115c792155a51
bGraebener <bastiangraebener@gmail.com>
2017-11-06 12:58:59 +0000

    change html

--- res/script.js
+++ res/script.js
@@ -38,8 +38,8 @@ function askQuestion() {
             if (xhr.responseText.length > 0) {
 
                 // append new text to the textarea and keep new text in view
-                textarea.value += "\n" + userName + ": " + userText + "\nEliza: " + xhr.responseText;
-                textarea.scrollTop = textarea.scrollHeight;
+                createListItem(userName, userText);
+                createListItem("Eliza", xhr.responseText);
 
                 // disable the submit button if the user quit the program
                 questionButton.disabled = xhr.getResponseHeader("quit") === "true";
@@ -47,6 +47,55 @@ function askQuestion() {
             }
         }
     }
+}
+
+var counter = 1;
+
+function createListItem(name, msg) {
+
+    var chatbox = document.getElementById("chatbox");
+
+    var newList = document.createElement("li");
+    newList.className = "w3-bar";
+    newList.style.border = "none";
+
+    var img = document.createElement("img");
+    img.style.width = "85px";
+    img.className = "w3-bar-item w3-circle";
+    img.src = "female_avatar.png";
+
+    var div = document.createElement("div");
+    div.className = "w3-bar-item";
+
+    var spanOne = document.createElement("span");
+    spanOne.className = "w3-large";
+    spanOne.innerText = name;
+    var spanTwo = document.createElement("span");
+    spanTwo.innerText = msg;
+
+    var br = document.createElement("br");
+
+
+    div.appendChild(spanOne);
+    div.appendChild(br);
+    div.appendChild(spanTwo);
+
+
+    if (counter % 2 === 0) {
+        newList.appendChild(img);
+        newList.appendChild(div);
+    } else {
+        newList.appendChild(img);
+        newList.appendChild(div);
+
+        div.style = "float:right";
+        img.style = "float:right; width:85px";
+        newList.style.textAlign = "right";
+    }
+    chatbox.appendChild(newList);
+    counter++;
+
+    window.scrollTo(0, document.body.scrollHeight);
 }
 // if user input text field is in focus allow enter key to trigger the ask question function
 document.getElementById("userTextInput").addEventListener("keypress", (event) => {
